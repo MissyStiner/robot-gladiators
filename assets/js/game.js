2182b83c07ecc3d18eca64526eff19d5a4e00ad7
Missy Stiner <missystiner@gmail.com>
2021-06-28 19:00:36 -0500

    at // What is this? section

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! JavaScript is running!");
+function fight() {
+    window.alert("The fight has begun!");
+}
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+
+
+
+
+// fight();
